f763fb06974052da9ddf4b3bb5f8b99284391de2
Precha Krasaesup <prechakra@counterservice.co.th>
2024-11-22 10:33:49 +0700

    fetch data api

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,21 @@
-const aboutPage = () => {
+const url = "https://jsonplaceholder.typicode.com/todos";
+
+const fetchTodos = async () => {
+  const res = await fetch(url);
+  const data = await res.json();
+  // console.log(data);
+  return data;
+};
+const aboutPage = async () => {
+  const todos = await fetchTodos();
+  console.log(todos);
+
   return (
     <div>
       <h1>About Page</h1>
+      {todos.map((item: any, index: number) => {
+        return <li key={index}>{item.title}</li>;
+      })}
     </div>
   );
 };
